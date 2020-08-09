576b48be7a1cc485e0fc06fc80c9ceae2234a627
ANdres espejo <jeaespejogo@unal.edu.co>
2020-08-08 22:07:13 -0500

    agregando categorias

---
+++ src/components/GifExpertApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Fragment from "react-dom";
+
+export const GifExpertApp = () => {
+    const categories = ["One Piece", "Dragon Ball", "Naruto"];
+    return (
+        <React.Fragment>
+            <h2>GifExpertApp</h2>
+            <hr />
+            <ul>
+				{categories.map}
+			</ul>
+        </React.Fragment>
+    );
+};

32f16030550679636e6eced156dc9d2d8aa98700
ANdres espejo <jeaespejogo@unal.edu.co>
2020-08-09 23:00:02 -0500

    comunicaciones en el form

--- src/components/GifExpertApp.jsx
+++ src/components/GifExpertApp.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import Fragment from "react-dom";
+import { AddCategory } from "./AddCategory";
 
 export const GifExpertApp = () => {
-    const categories = ["One Piece", "Dragon Ball", "Naruto"];
+    // const categories = ["One Piece", "Dragon Ball", "Naruto"];
+    const [categories, setCategories] = useState([
+        "One Piece",
+        "Dragon Ball",
+        "Naruto",
+    ]);
+    // const handleAdd = () => {
+    //     setCategories(["Bleach", ...categories]);
+    // };
     return (
         <React.Fragment>
             <h2>GifExpertApp</h2>
+            <AddCategory />
             <hr />
             <ul>
-				{categories.map}
-			</ul>
+                {categories.map((category) => {
+                    return <li key={category}>{category}</li>;
+                })}
+            </ul>
         </React.Fragment>
     );
 };

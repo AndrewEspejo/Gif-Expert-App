94ae1f8ee31e34b86fe56d851857b0c2ba2ce2c0
ANdres espejo <jeaespejogo@unal.edu.co>
2020-08-23 02:38:16 -0500

    Updates

---
+++ precache-manifest.e424d61a1703a77b845de2fa5acd4d99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34480b00be65f615909f285915e1f6aa",
+    "url": "/Gif-Expert-App%20/index.html"
+  },
+  {
+    "revision": "65fbd5703c519fa0646e",
+    "url": "/Gif-Expert-App%20/static/css/main.3fff39fd.chunk.css"
+  },
+  {
+    "revision": "a376457046248fb21976",
+    "url": "/Gif-Expert-App%20/static/js/2.ee458b5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gif-Expert-App%20/static/js/2.ee458b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fbd5703c519fa0646e",
+    "url": "/Gif-Expert-App%20/static/js/main.c43df623.chunk.js"
+  },
+  {
+    "revision": "f4fdfb8f3f398b6ff924",
+    "url": "/Gif-Expert-App%20/static/js/runtime-main.be965a47.js"
+  }
+]);
